0373f0caa5cecdb0377679d0afbf4e283376ced2
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-12-13 08:30:12 +0100

    Added rule to rename ContaoFrameworkInterface to ContaoFramework

--- config/sets/contao/contao-49.php
+++ config/sets/contao/contao-49.php
@@ -2,10 +2,18 @@
 
 declare(strict_types=1);
 
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\Framework\ContaoFrameworkInterface;
 use Rector\Config\RectorConfig;
 use Rector\Renaming\Rector\FuncCall\RenameFunctionRector;
+use Rector\Renaming\Rector\Name\RenameClassRector;
 
 return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
+        // Deprecated in Contao 4.1
+        ContaoFrameworkInterface::class => ContaoFramework::class,
+    ]);
+
     $rectorConfig->ruleWithConfiguration(RenameFunctionRector::class, [
         // Added in Contao 4.1
         'utf8_convert_encoding' => 'Contao\StringUtil::convertEncoding',
