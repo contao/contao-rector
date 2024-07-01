a517f35f367c8598955318df7ff869edf8def926
Andreas Schempp <andreas.schempp@terminal42.ch>
2024-07-01 08:53:49 +0200

    Added Symfony 6.4 to the Contao 5.3 level set

--- config/sets/contao/level/up-to-contao-53.php
+++ config/sets/contao/level/up-to-contao-53.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 use Contao\Rector\Set\ContaoLevelSetList;
 use Contao\Rector\Set\ContaoSetList;
 use Rector\Config\RectorConfig;
+use Rector\Symfony\Set\SymfonySetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
         ContaoLevelSetList::UP_TO_CONTAO_51,
         ContaoSetList::CONTAO_53,
+        SymfonySetList::SYMFONY_60,
+        SymfonySetList::SYMFONY_61,
+        SymfonySetList::SYMFONY_62,
+        SymfonySetList::SYMFONY_63,
+        SymfonySetList::SYMFONY_64,
     ]);
 };
