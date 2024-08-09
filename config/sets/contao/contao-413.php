d88111479d10f0710b5c9ea4c6e531b3ca481ee4
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-08-10 00:00:32 +0200

    Finish merge

--- config/sets/contao/contao-413.php
+++ config/sets/contao/contao-413.php
@@ -13,10 +13,14 @@ use Contao\Controller;
 use Contao\CoreBundle\Framework\ContaoFrameworkInterface;
 use Contao\CoreBundle\Security\ContaoCorePermissions;
 use Contao\CoreBundle\Util\SimpleTokenParser;
+use Contao\DC_File;
+use Contao\DC_Folder;
+use Contao\DC_Table;
 use Contao\Folder;
 use Contao\Rector\Rector\ConstantToServiceCallRector;
 use Contao\Rector\Rector\InsertTagsServiceRector;
 use Contao\Rector\Rector\LegacyFrameworkCallToServiceCallRector;
+use Contao\Rector\Rector\ReplaceDataContainerRector;
 use Contao\Rector\Rector\SystemLanguagesToServiceRector;
 use Contao\Rector\ValueObject\ConstantToServiceCall;
 use Contao\Rector\ValueObject\LegacyFrameworkCallToServiceCall;
@@ -32,6 +36,7 @@ use Rector\Transform\Rector\FuncCall\FuncCallToStaticCallRector;
 use Rector\Transform\Rector\StaticCall\StaticCallToFuncCallRector;
 use Rector\Transform\ValueObject\FuncCallToStaticCall;
 use Rector\Transform\ValueObject\StaticCallToFuncCall;
+use Rector\Transform\ValueObject\StringToClassConstant;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 return static function (RectorConfig $rectorConfig): void {
