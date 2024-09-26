858d3f804bce2a653a497327bfcde6eabbe4507f
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-09-26 20:11:44 +0200

    Add test for `validImageTypes` to 'valid_extensions'

--- tests/Rector/ReplaceNestedArrayItemRector/config/config.php
+++ tests/Rector/ReplaceNestedArrayItemRector/config/config.php
@@ -2,17 +2,24 @@
 
 declare(strict_types=1);
 
-use Contao\DC_File;
-use Contao\DC_Folder;
-use Contao\DC_Table;
 use Contao\Rector\Rector\ReplaceNestedArrayItemRector;
 use Contao\Rector\ValueObject\ReplaceNestedArrayItemValue;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\StaticCall;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Scalar\String_;
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->ruleWithConfiguration(ReplaceNestedArrayItemRector::class, [
-        new ReplaceNestedArrayItemValue('TL_DCA.*.*.dataContainer','Table', DC_Table::class),
-        new ReplaceNestedArrayItemValue('TL_DCA.*.config.dataContainer','Folder', DC_Folder::class),
-        new ReplaceNestedArrayItemValue('TL_DCA.tl_baz.config.dataContainer','File', DC_File::class),
+        new ReplaceNestedArrayItemValue('TL_DCA.*.*.dataContainer', 'Table', \Contao\DC_Table::class),
+        new ReplaceNestedArrayItemValue('TL_DCA.*.config.dataContainer','Folder', \Contao\DC_Folder::class),
+        new ReplaceNestedArrayItemValue('TL_DCA.tl_baz.config.dataContainer','File', \Contao\DC_File::class),
+
+        new ReplaceNestedArrayItemValue(
+            'TL_DCA.*.fields.*.eval.extensions',
+            new StaticCall(new FullyQualified(Config::class), 'get', [new Arg(new String_('validImageTypes'))]),
+            '%contao.image.valid_extensions%'
+        ),
     ]);
 };
