4ddff13389ea64e3b3413bcb7a874e7b7e0a6e7a
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-04-11 01:10:58 +0200

    Rector `System::getCountries` to a method call (#26)

--- src/Rector/ReplaceNestedArrayItemRector.php
+++ src/Rector/ReplaceNestedArrayItemRector.php
@@ -8,7 +8,6 @@ use Contao\Config;
 use Contao\Rector\ValueObject\ReplaceNestedArrayItemValue;
 use PhpParser\Node;
 use PhpParser\Node\Arg;
-use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Expr\ArrayDimFetch;
 use PhpParser\Node\Expr\ArrayItem;
@@ -303,7 +302,7 @@ CODE_AFTER
                 ) {
                     if (
                         [] !== $childrenKeyPath
-                        && $sub->key->value === array_values($childrenKeyPath)[0] ?? null
+                        && $sub->key->value === (array_values($childrenKeyPath)[0] ?? null)
                     ) {
                         array_shift($childrenKeyPath);
                         $this->replaceTargetNodeValue($sub, $childrenKeyPath, $configuration, $oldValue, $newValue);
@@ -311,6 +310,7 @@ CODE_AFTER
 
                     elseif (
                         [] === $childrenKeyPath
+                        && $sub->key->value === (array_values($childrenKeyPath)[0] ?? null)
                         && $this->matchesReplacementValue($sub, $oldValue)
                     ) {
                         $sub = $newValue;
--- tests/Rector/ReplaceNestedArrayItemRector/config/config.php
+++ tests/Rector/ReplaceNestedArrayItemRector/config/config.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 use Contao\DataContainer;
 use Contao\Rector\Rector\ReplaceNestedArrayItemRector;
 use Contao\Rector\ValueObject\ReplaceNestedArrayItemValue;
+use Contao\System;
 use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\ClassConstFetch;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\StaticCall;
+use PhpParser\Node\Name;
 use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Scalar\String_;
 use Rector\Config\RectorConfig;
@@ -29,5 +33,24 @@ return static function (RectorConfig $rectorConfig): void {
             1,
             new ClassConstFetch(new FullyQualified(DataContainer::class), 'SORT_INITIAL_LETTER_ASC')
         ),
+
+        new ReplaceNestedArrayItemValue(
+            'TL_DCA.*.fields.*.options',
+            new StaticCall(new FullyQualified(System::class), 'getCountries'),
+            new FuncCall(
+                new Name('array_change_key_case'), [
+                    new Arg(
+                        new MethodCall(
+                            new MethodCall(
+                                new StaticCall(new FullyQualified(System::class), 'getContainer'),
+                                'get',
+                                [new Arg(new String_('contao.intl.countries'))]
+                            ),
+                            'getCountries'
+                        )
+                    )
+                ]
+            )
+        )
     ]);
 };
