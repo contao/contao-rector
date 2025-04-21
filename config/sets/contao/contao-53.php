2d0af2ddd17eefeb19483c6605e050ff3e3ae6b7
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-04-21 08:53:50 +0200

    Change `ModelMetadataTrait` to `MetadataTrait` (#25)

--- config/sets/contao/contao-53.php
+++ config/sets/contao/contao-53.php
@@ -2,10 +2,13 @@
 
 declare(strict_types=1);
 
+use Contao\CoreBundle\File\ModelMetadataTrait;
 use Contao\CoreBundle\Security\ContaoCorePermissions;
+use Contao\Model\MetadataTrait;
 use Contao\Rector\Rector\StringReplaceRector;
 use Rector\Config\RectorConfig;
 use Rector\Renaming\Rector\ClassConstFetch\RenameClassConstFetchRector;
+use Rector\Renaming\Rector\Name\RenameClassRector;
 use Rector\Renaming\ValueObject\RenameClassAndConstFetch;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -16,5 +19,9 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->ruleWithConfiguration(StringReplaceRector::class, [
         '_legend:hide}',
         '_legend:collapsed}'
+    ]);
+
+    $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
+        ModelMetadataTrait::class => MetadataTrait::class
     ]);
 };
